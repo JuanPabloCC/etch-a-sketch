d48b3047e77ae25cb53679601a3d89639092529b
Juan Pablo <juan.pablo.castano@outlook.com>
2025-06-28 11:21:05 -0500

    feat: add eventlistener to containers

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ for (let step = 1; step < 17; step++) {
     let container = document.createElement("div");
     container.textContent = `I'm number ${step}`;
     container.classList.add("container");
+    container.addEventListener("mouseover", () => container.classList.add("hover"));
     gridContainer.appendChild(container);
 }
