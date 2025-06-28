c62f87cfd2133f84a1184e285cab342133e3b7b4
Juan Pablo <juan.pablo.castano@outlook.com>
2025-06-28 11:04:39 -0500

    feat: add class container to new elements

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ let gridContainer = document.querySelector("#grid-container");
 for (let step = 1; step < 17; step++) {
     let container = document.createElement("div");
     container.textContent = `I'm number ${step}`;
+    container.classList.add("container");
     gridContainer.appendChild(container);
 }
