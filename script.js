661c3af3bea2a5b6523b2d982b1554c5ab398f35
Juan Pablo <juan.pablo.castano@outlook.com>
2025-06-28 10:54:17 -0500

    feat: append 16 divs to grid container

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 let gridContainer = document.querySelector("#grid-container");
 
+for (let step = 1; step < 17; step++) {
+    let container = document.createElement("div");
+    container.textContent = `I'm number ${step}`;
+    gridContainer.appendChild(container);
+}
