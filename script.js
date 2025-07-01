012cc6bb9ceb58245a721518b43917228ee3026d
Juan Pablo <juan.pablo.castano@outlook.com>
2025-07-01 13:19:20 -0500

    feat: add random color function for squares

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let gridContainer = document.querySelector("#grid-container");
 let startButton = document.querySelector("#start-button");
 
+function rgbColor() {
+    let red = Math.floor(Math.random()*255);
+    let blue = Math.floor(Math.random()*255);
+    let green = Math.floor(Math.random()*255);
+    return `rgb(${red}, ${blue}, ${green})`
+}
+
 startButton.addEventListener("click", () => {
     gridContainer.innerHTML = "";
     let numberOfSquares = +prompt("Please enter the number of squares (1 - 100)");
@@ -11,7 +18,8 @@ startButton.addEventListener("click", () => {
         container.classList.add("container");
         container.style.width = squareWidth+"px";
         container.style.height = squareWidth+"px";
-        container.addEventListener("mouseover", () => container.classList.add("hover"));
+        let color = rgbColor();
+        container.addEventListener("mouseover", () => container.style.backgroundColor = color);
         gridContainer.appendChild(container);
     }
     gridContainer.setAttribute("style", "border: dotted 2px red");
