91f5b1139063341331d87438fe6af717b96f45c2
Juan Pablo <juan.pablo.castano@outlook.com>
2025-07-01 11:05:01 -0500

    feat: add extended funcionality to start button
    Now the user can select a number of squares per side
    and a canvas of such dimensions will be drawn.

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 let gridContainer = document.querySelector("#grid-container");
-
-for (let step = 1; step < 17; step++) {
-    let container = document.createElement("div");
-    container.textContent = `I'm number ${step}`;
-    container.classList.add("container");
-    container.addEventListener("mouseover", () => container.classList.add("hover"));
-    gridContainer.appendChild(container);
-}
-
 let startButton = document.querySelector("#start-button");
 
-startButton.addEventListener("click", () => prompt("Please enter the number of squares (max 100)"));
+startButton.addEventListener("click", () => {
+    let numberOfSquares = +prompt("Please enter the number of squares (1 - 100)");
+    let totalNumberOfSquares = numberOfSquares**2;
+    let squareWidth = 800/numberOfSquares;
+    for (let step = 1; step <= totalNumberOfSquares; step++) {
+        let container = document.createElement("div");
+        container.textContent = `I'm number ${step}`;
+        container.classList.add("container");
+        container.style.width = squareWidth+"px";
+        container.style.height = squareWidth+"px";
+        container.addEventListener("mouseover", () => container.classList.add("hover"));
+        gridContainer.appendChild(container);
+    }
+});
