19db6471ffd51c15781fb87e01a6af632044df2a
Juan Pablo <juan.pablo.castano@outlook.com>
2025-07-01 17:10:51 -0500

    feat: add drag mouse feature to paint
    now the user can drag the cursor to paint and lift
    it to stop painting, this makes a nice user experience
    and allows more complex drawings.

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ function rgbColor() {
     return `rgb(${red}, ${blue}, ${green})`
 }
 
+function changeColor(div){
+    div.target.style.backgroundColor = rgbColor();
+}
+
 startButton.addEventListener("click", () => {
     gridContainer.innerHTML = "";
     let numberOfSquares = +prompt("Please enter the number of squares (1 - 100)");
@@ -18,12 +22,17 @@ startButton.addEventListener("click", () => {
         gridContainer.appendChild(container);
         container.style.width = squareWidth+"px";
         container.style.height = squareWidth+"px";
-        let color = rgbColor();
-        container.addEventListener("mouseover", () => {
-            container.style.backgroundColor = color;
-        });
-        
     }
     gridContainer.setAttribute("style", "border: dotted 2px red");
 });
+
+gridContainer.addEventListener("mousedown", () => {
+    let gridChilds = gridContainer.querySelectorAll("div");
+    gridChilds.forEach(child => child.addEventListener("mousemove", changeColor));
+});
+
+gridContainer.addEventListener("mouseup", () => {
+    let gridChilds = gridContainer.querySelectorAll("div");
+    gridChilds.forEach(child =>child.removeEventListener("mousemove", changeColor));
+})
 
